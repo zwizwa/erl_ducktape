f21757951500ea79c7bf30e03d5210677b309ea5
root <root@core.pub.k>
2017-07-24 18:01:00 -0400

    allow old kodi interface through http also

--- src/kodi.erl
+++ src/kodi.erl
@@ -1,121 +1,37 @@
 -module(kodi).
 -export([%% server
-         start_link/0, connected/2, cmd/2,
-         activeplayers/1, playerid/1, connect/1, proxy/1, proxy_tcp/2,
+         cmd/2,
+         activeplayers/1, playerid/1,
          %% single message
          http_jsonrpc/3, notify_scan/1]).
 -ifdef(TEST).
 -include_lib("eunit/include/eunit.hrl").
 -endif.
 
-start_link() ->
-    Pid = spawn_link(fun not_connected/0),
-    register(kodi, Pid),
-    {ok, Pid}.
-
-
-connect_init(Host) ->
-    connect_init(Host, fun() -> ignore end).
-connect_init(Host, Init) ->
-    TcpPort = 9090,
-    case gen_tcp:connect(Host, TcpPort, [binary, {active, true}]) of
-        {ok, Port} ->
-            Init(),
-            connected(Port, #{});
-        Error ->
-            tools:info("error: ~p~n", [Error]),
-            not_connected()
-    end.
-
-not_connected() ->
-    receive 
-        {proxy, Node}   -> proxy_init(Node);
-        {connect, Host} -> connect_init(Host);
-        %% By default, start on localhost.
-        Msg -> connect_init("ip6-localhost", fun() -> self() ! Msg end)
-    end.
-
-
-%% Also plug local port, e.g.:
-%% ssh -L 8080:localhost:8080 pi2.vpn.k
-%% Or start an erlang server process.
-%%     spawn_link(fun() -> os:cmd(tools:format("ssh -L 8080:localhost:8080")) ...
-
-
-%% Proxy TCP over erlang distro network.
-proxy_tcp(Node,Port) ->
-    serv:start(
-      {handler,
-       fun() ->
-               serv_tcp:init(
-                 [Port],
-                 %% Connect init + handler
-                 {handler,
-                  fun(Sock,_) -> #{sock => Sock, node => Node} end,
-                  fun proxy_tcp_handle/2},
-                 %% Registry message handler
-                 fun(_, _) -> void end, void)
-       end}).
-
-proxy_tcp_handle(Msg, State) ->                 
-    tools:info("~p~n",[Msg]),
-    State.
+%% Two mechanism are available:
+%% - An Erlang server process
+%% - A function, using curl for message delivery over http.
+%%
+%% The latter doesn't need OTP and can be run from an escript.
+%%
+%% A server is only necessary to handle notifications.  Currently I
+%% have no use case for this, so the server is being moved elsewhere.
 
 
 
-proxy_init(Node) when is_atom(Node) ->
-    proxy_loop(rpc:call(Node,erlang,whereis,[kodi])).
-proxy_loop(Proxy) when is_pid(Proxy) ->
-    receive
-        {proxy, Node}   -> proxy_init(Node);
-        {connect, Host} -> connect_init(Host);
-        Msg -> Proxy ! Msg, proxy_loop(Proxy)
-    end.        
+%% JSONRPC notes:
 
-connected(Port, State) ->
-    receive
-        %% FIXME: there is no guarantee Bin contains a full message!
-        {tcp, Port, Bin} ->
-            NextTrailer = handle_bin(iolist_to_binary([maps:get(trailer, State, <<>>),Bin])),
-            kodi:connected(Port, maps:put(trailer, NextTrailer, State));
-        {command, Pid, Fun} ->
-            {NewState, Json} = Fun(State),
-            JsonId = jiffy:encode(maps:put(<<"id">>,pid_enc(Pid),Json)),
-            gen_tcp:send(Port, JsonId),
-            kodi:connected(Port, NewState);
-        {connect, Host} ->
-            gen_tcp:close(Port),
-            connect_init(Host, fun() -> ignore end);
-        Other ->
-            io:format("unknown: ~p~n", [Other]),
-            exit(Other)
-            %%kodi:connected(Port, State)
-    end.
-handle_json(Json) ->
-    case maps:get(<<"id">>, Json, notification) of
-        notification ->
-            %% io:format("notification:~n  ~p~n", [Json]),
-            ignore;
-        PidEnc ->
-            %% io:format("reply:~n  ~p~n", [Json]),
-            pid_dec(PidEnc) ! {reply, Json}
-    end.
-handle_bin(Bin) ->
-    case jiffy:decode(Bin, [return_maps, return_trailer]) of
-        {error, {_, truncated_json}} ->
-            Bin;
-        {has_trailer, Json, Rest} ->
-            handle_json(Json),
-            handle_bin(Rest);
-        Json -> 
-            handle_json(Json),
-            <<>>
-    end.
+%% - id: absence of id means it is a notification: no reply is sent.
+%%   otherwise, a reply always sent.  the exception seems to be
+%%   badly structured mesages such as '{}'.
 
 
-%% This is not proper.. but works.
-pid_enc(X) -> list_to_binary(io_lib:format("~p",[X])).
-pid_dec(X) -> list_to_pid(binary_to_list(X)).
+
+
+
+
+
+
 
 
 rpc(Method, Params) -> #{
@@ -124,7 +40,17 @@ rpc(Method, Params) -> #{
      <<"method">>  => Method,
      <<"params">>  => Params
     }.
-                            
+
+%% For some actions we can go through stateless http.  In this case
+%% all actions that depend on state will use only their defaults.
+call({http,Host}, Fun) ->     
+    FakeState = #{},
+    {_IgnoredState, EJson} = Fun(FakeState),
+    http_jsonrpc(Host, EJson, 3000);
+    
+%% Full server interface.
+%% Server will run: {NewState, Json} = Fun(State),
+%% And will return an EJson reply.
 call(K, Fun) ->
     K ! {command, self(), Fun},
     receive 
@@ -185,12 +111,6 @@ playerid(K) -> hd(activeplayers(K) ++ [-1]).  %% don't crash in case nothing is 
 
 clip(X) -> max(0,min(100,X)).
 
-
-%% Connect to kodi directly.
-connect(Host)  -> kodi ! {connect, Host}.
-%% Connect to different node.
-proxy(Node) when is_atom(Node) -> kodi ! {proxy, Node};
-proxy(Host) -> proxy(list_to_atom(tools:format("exo@~s.vpn.k",[Host]))).
      
 
 %% =========================================================================
@@ -215,6 +135,7 @@ http_jsonrpc(Host, EJson, TimeOut) ->
     PW = pw("kodi"),
     URL = tools:format_binary("http://kodi:~s@~s:8080/jsonrpc",[PW,Host]),
     Bin = http_post(URL, "application/json", jiffy:encode(EJson), TimeOut),
+    %% io:format("~p~n",[{URL,Bin}]),
     jiffy:decode(Bin).                 
 
 pw(File) ->
