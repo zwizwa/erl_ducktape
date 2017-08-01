dcf2d629e5563d7c9130fb4604a0eed44811391d
root <root@core.pub.k>
2017-08-01 14:55:02 -0400

    ops:distribution

--- src/ops.erl
+++ src/ops.erl
@@ -1,7 +1,7 @@
 %% System level tools, e.g. linux ssh access.
 -module(ops).
 -export([ssh/3, cmd/1,
-         hp/1,hs/1]).
+         hp/1,hs/1,set_cookie/1,distribution/2]).
 
     
 
@@ -29,3 +29,16 @@ hs(F) -> lists:map(F, hosts()).
 %    {ok, {El, _}} = tools:script_xml("nmap -oX - -sn 10.71.71.0/24", 20000),
 %    lists:map(fun tools:xmlElement_attributes_proplist/1,
 %              xmerl_xpath:string("//nmaprun/host/address",El)).
+
+
+set_cookie({pw,Name}) ->
+    set_cookie({file,"~/.pw/" ++ Name});
+set_cookie({file,File}) ->
+    {ok, Bin} = file:read_file(File),
+    set_cookie(Bin);
+set_cookie(Bin) when is_binary(Bin) ->
+    erlang:set_cookie(node(), binary_to_atom(Bin, utf8)).
+
+distribution(Name, Cookie) ->
+    net_kernel:start([Name, longnames]),
+    set_cookie(Cookie).
